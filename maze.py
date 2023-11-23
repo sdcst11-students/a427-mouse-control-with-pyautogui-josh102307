b03deb4d7632884a8b740479a7f0c5ecd9de02e1
josh102307 <144732172+josh102307@users.noreply.github.com>
2023-11-23 13:30:20 -0800

    i did the assignment

---
+++ maze.py
@@ -0,0 +1,26 @@
+import pyautogui, sys
+import time
+
+
+pyautogui.moveTo(1012,184,1)
+pyautogui.moveTo(1080,184,1)
+pyautogui.moveTo(1080,258,1)
+pyautogui.moveTo(1058,258,1)
+pyautogui.moveTo(1058,346,1)
+pyautogui.moveTo(1104,348,1)
+pyautogui.moveTo(1104,326,1)
+pyautogui.moveTo(1084,326,1)
+pyautogui.moveTo(1084,280,1)
+pyautogui.moveTo(1129,280,1)
+pyautogui.moveTo(1129,235,1)
+pyautogui.moveTo(1172,235,1)
+pyautogui.moveTo(1172,210,1)
+pyautogui.moveTo(1218,210,1)
+pyautogui.moveTo(1218,255,1)
+pyautogui.moveTo(1178,255,1)
+pyautogui.moveTo(1178,325,1)
+pyautogui.moveTo(1151,325,1)
+pyautogui.moveTo(1151,395,1)
+pyautogui.moveTo(1194,395,1)
+pyautogui.moveTo(1194,371,1)
+pyautogui.moveTo(1296,371,1)
